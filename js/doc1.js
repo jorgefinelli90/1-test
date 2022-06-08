e1f8373d1eae154b34acc25e7fa716815032d4b6
Jorge Finelli <jorgefinelli90@gmail.com>
2022-06-07 21:16:59 -0300

    test 1er clase

---
+++ js/doc1.js
@@ -0,0 +1,17 @@
+let nombre = prompt("ingrese su nombre")
+let apellido = prompt("ingrese el apellido")
+
+
+
+let suma = nombre + " " + apellido
+
+alert("hola  " + suma + " me conto tu novio que lo amas mucho") 
+
+let cantidad = prompt("del 1 al 10, cuanto lo amas?" ) 
+
+alert("woww " + cantidad + " es bastante no?")
+
+let lamor = prompt(nombre + apellido + "queres hacer lamor conmigo")
+
+alert("gracias " + nombre + " por contestar afirmativamente, adios")
+
