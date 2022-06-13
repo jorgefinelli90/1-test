e19ad6fea555a194b10f73f7fc639f5280152296
Jorge Finelli <jorgefinelli90@gmail.com>
2022-06-12 23:07:01 -0300

    .

---
+++ js/doc2.js
@@ -0,0 +1,8 @@
+let calificacionUno = prompt("Ingrese su primer calificacion")
+let calificacionDos = prompt("Ingrese su segunda calificacion")
+
+if(calificacionUno >=6 || calificacionDos){
+    alert("Aprobado!")
+}else{
+    alert("Reprobado!")
+}
